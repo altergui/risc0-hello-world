4a1a8a5af3e105ec150e87d8e2bb50ad872857ef
Gui Iribarren <gui@altermundi.net>
2024-09-26 11:41:57 +0200

    compute fibonacci in guest

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,13 +1,19 @@
 use risc0_zkvm::guest::env;
 
 fn main() {
-    // TODO: Implement your guest code here
-
     // read the input
     let input: u32 = env::read();
 
-    // TODO: do something with the input
+    // Compute the n'th fibonacci number, using normal Rust code.
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..input {
+        let mut c = a + b;
+        c %= 7919; // Modulus to prevent overflow.
+        a = b;
+        b = c;
+    }
 
     // write public output to the journal
-    env::commit(&input);
+    env::commit(&b);
 }
